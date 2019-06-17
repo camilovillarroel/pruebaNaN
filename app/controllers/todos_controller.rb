2db2a14259fd0192c1095b7c38bd0d0b1707b728
Camilo Villarroel Perez <camiloed92@hotmail.com>
2019-06-17 17:53:58 -0400

    last commit?

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,13 +4,14 @@ class TodosController < ApplicationController
     def index
         @todos = Todo.all
 
-        
     end
+    
     def show
         @todo = Todo.find(params[:id])
-        @usertodo = UserTodo.find(
-            params[:todo_id],
-            current_user.id 
-         ).done 
+        @usertodo = UserTodo.find_by(
+            todo_id: params[:id],
+            user_id: current_user.id 
+         )
+        @users_array = User.all
     end
 end
--- app/controllers/user_todos_controller.rb
+++ app/controllers/user_todos_controller.rb
@@ -1,19 +1,19 @@
 class UserTodosController < ApplicationController
 
     def set_done
-        task_done = UserTodo.new({
+        task_done = UserTodo.find_by(
             todo_id: params[:todo_id],
             user_id: current_user.id 
-         })
+        )
         task_done.done = true
         task_done.save
         redirect_to todos_path
     end
     def set_undone
-        task_done = UserTodo.new({
+        task_done = UserTodo.find_by(
             todo_id: params[:todo_id],
             user_id: current_user.id 
-         })
+        )
         task_done.done = false
         task_done.save
         redirect_to todos_path
